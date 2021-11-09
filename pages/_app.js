14469886336cd2b8e3fae09da562f5fce55c103f
D.V.D <entropyhacker@gmail.com>
2021-11-09 09:43:39 +0200

    feat: add _app.js and boostwatch

---
+++ pages/_app.js
@@ -0,0 +1,17 @@
+import TagManager from "react-gtm-module"
+import "bootswatch/dist/litera/bootstrap.min.css"
+//import "../styles/index.scss";
+
+if (typeof window !== "undefined" && process.env.NEXT_PUBLIC_GTM_ID) {
+  TagManager.initialize({ gtmId: process.env.NEXT_PUBLIC_GTM_ID })
+}
+
+function MyApp({ Component, pageProps }) {
+  return (
+    <div className="container">
+      <Component {...pageProps} />
+    </div>
+  );
+}
+
+export default MyApp
