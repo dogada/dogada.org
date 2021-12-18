fcc33afbb59147cf6bcf5fef52f37e24181bc254
D.V.D <entropyhacker@gmail.com>
2021-12-18 14:07:30 +0200

    chore: settings

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,6 @@
 import TagManager from "react-gtm-module"
 import "bootswatch/dist/litera/bootstrap.min.css"
-//import "../styles/index.scss";
+//import "../styles/index.scss"
 
 if (typeof window !== "undefined" && process.env.NEXT_PUBLIC_GTM_ID) {
   TagManager.initialize({ gtmId: process.env.NEXT_PUBLIC_GTM_ID })
@@ -11,7 +11,7 @@ function MyApp({ Component, pageProps }) {
     <div className="container">
       <Component {...pageProps} />
     </div>
-  );
+  )
 }
 
 export default MyApp
