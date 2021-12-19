daae13a951bb10c89ffa0b2822df0c5c2d2f9bc7
D.V.D <entropyhacker@gmail.com>
2021-12-19 13:56:40 +0200

    feat: add layout and title

---
+++ core/Layout.jsx
@@ -0,0 +1,16 @@
+import Head from 'next/head'
+
+export function Layout ({meta, children}) {
+  return (
+  <div>
+    <Head>
+      <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1, shrink-to-fit=no"
+      />      
+      <title>{meta.title } · {process.env.NEXT_PUBLIC_SITE_DOMAIN}</title>
+    </Head>
+    {children}
+  </div>
+  )
+}
